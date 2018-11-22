ed3d9b008edb496bae7c08723b3b09ea094c6b29
bghgu <ektmf1993@gmail.com>
2018-11-22 23:25:10 +0900

    edit report 2

--- 2차/report2/src/main/java/org/sopt/report2/api/UserController.java
+++ 2차/report2/src/main/java/org/sopt/report2/api/UserController.java
@@ -41,19 +41,48 @@ public class UserController {
      *
      * @return 회원 리스트
      */
+//    @GetMapping("/users")
+//    public String getUserList(
+//            @RequestParam(value = "name", required = false) final Optional<String> name,
+//            @RequestParam(value = "part", required = false) final Optional<String> part) {
+//        if (name.isPresent()) {
+//            for (User u : userList) {
+//                if (u.getName().equals(name.get())) return u.toString();
+//            }
+//            return "없습니다";
+//        }
+//        if (part.isPresent()) {
+//            for (User u : userList) {
+//                if (u.getPart().equals(part.get())) return u.toString();
+//            }
+//            return "없습니다";
+//        } else {
+//            if(userList.isEmpty()) return "리스트가 비었습니다.";
+//            else {
+//                StringBuilder stringBuilder = new StringBuilder();
+//                for (User u : userList) {
+//                    stringBuilder.append(u.toString()).append("\n");
+//                }
+//                return stringBuilder.toString();
+//            }
+//        }
+//    }
+
     @GetMapping("/users")
     public String getUserList(
-            @RequestParam(value = "name", required = false) final Optional<String> name,
-            @RequestParam(value = "part", required = false) final Optional<String> part) {
-        if (name.isPresent()) {
+            @RequestParam(value = "name", required = false) final String name,
+            @RequestParam(value = "part", required = false) final String part) {
+        if (name != null) {
+            System.out.println(name);
             for (User u : userList) {
-                if (u.getName().equals(name.get())) return u.toString();
+                if (u.getName().equals(name)) return u.toString();
             }
             return "없습니다";
         }
-        if (part.isPresent()) {
+        if (part != null) {
+            System.out.println(part);
             for (User u : userList) {
-                if (u.getPart().equals(part.get())) return u.toString();
+                if (u.getPart().equals(part)) return u.toString();
             }
             return "없습니다";
         } else {
--- 2차/report2/src/main/java/org/sopt/report2/model/User.java
+++ 2차/report2/src/main/java/org/sopt/report2/model/User.java
@@ -6,11 +6,11 @@ package org.sopt.report2.model;
 
 public class User {
     //회원 고유번호
-    private int user_idx;
+    private int user_idx = 0;
     //회원 이름
-    private String name;
+    private String name = "";
     //회원 파트
-    private String part;
+    private String part = "";
 
     public User() {}
 
