9ab3780d7bfaf366ea795a6707ce13ca08ca2499
bghgu <ektmf1993@gmail.com>
2018-12-12 12:25:27 +0900

    add seminiar 8

---
+++ 8차/seminar8/src/main/java/org/sopt/seminar8/Seminar8Application.java
@@ -0,0 +1,12 @@
+package org.sopt.seminar8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seminar8Application.class, args);
+    }
+}
